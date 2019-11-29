4381144f523cb1d13626ba09b7d9195f883000d5
DukhDmytro <cowboybebop4991@gmail.com>
2019-11-29 11:39:58 +0200

    1st commit

--- ways.py
+++ ways.py
@@ -1,6 +1,10 @@
+"""module"""
 from typing import Tuple
 
 
-def way(map: Tuple[str], city1:str, city2:str) -> bool:
-
-    return None
+def way(road: Tuple[str], city1: str, city2: str) -> bool:
+    """return is it possible to get from city X to city Y with given road"""
+    try:
+        return "".join(road).index(city1) < "".join(road).index(city2)
+    except ValueError:
+        return False

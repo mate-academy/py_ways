d6dc0308b1a5e7a53a21ebed868d5049bf1741f0
logotip <logotip123@yahoo.com>
2019-11-29 12:06:01 +0200

    v 1.0

--- ways.py
+++ ways.py
@@ -1,6 +1,21 @@
+"""ways search module"""
+
 from typing import Tuple
 
 
-def way(map: Tuple[str], city1:str, city2:str) -> bool:
-
-    return None
+def way(coord: Tuple[str], city1: str, city2: str) -> bool:
+    """
+    :param coord: coords map
+    :param city1: start city
+    :param city2: finish city
+    :return: True if can go from city 1 to city 2, else False
+    """
+    result = city1
+    for i in coord:
+        if i[0] == result:
+            result = i[-1]
+        if result == city2:
+            break
+    else:
+        return False
+    return True

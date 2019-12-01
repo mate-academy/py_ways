8979bb5c62b740acef7ae088119c391563bbb665
logotip <logotip123@yahoo.com>
2019-12-01 20:47:57 +0200

    v 1.2

--- ways.py
+++ ways.py
@@ -1,22 +1,28 @@
 """ways search module"""
 
-from typing import Tuple
+from typing import Tuple, List
 
 
-def way(coord: Tuple[str], city1: str, city2: str) -> bool:
+def way(coord_map: Tuple[str], city_1: str, city_2: str) -> bool:
     """
     :param coord: coords map
     :param city1: start city
     :param city2: finish city
     :return: True if can go from city 1 to city 2, else False
     """
-    result = city1
+    result = []  # type: List[str]
+    checker = city_1
+    index = 0
     while True:
-        last_result = result
-        for i in coord:
-            if i[0] == result:
-                result = i[-1]
-            if result == city2:
+        for coord in coord_map:
+            if coord[0] == checker and coord[-1] not in result:
+                result.append(coord[-1])
+            elif coord[-1] == checker and coord[0] not in result:
+                result.append(coord[0])
+            if city_2 in result:
                 return True
-        if last_result == result:
+        try:
+            checker = result[index]
+            index += 1
+        except IndexError:
             return False

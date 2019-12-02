0d25b524b80aa07801fc14ed577ab120484ffcd5
Dima <dima.sapsay@gmail.com>
2019-12-02 12:25:53 +0200

    third commit

--- ways.py
+++ ways.py
@@ -6,20 +6,13 @@ city X to city Y.
 
 
 from typing import Tuple
-import string
 
 
 def way(map_roads: Tuple[str], city1: str, city2: str) -> bool:
     """checking the map with cities"""
-
-    alphabets = string.ascii_uppercase
     str_roads = ''
 
     for road in map_roads:
-        str_roads += alphabets[alphabets.find(road[0]):
-                               alphabets.find(road[-1])]
+        str_roads += road[0] + road[-1]
 
-    check_road = alphabets[alphabets.find(city1[0]):
-                           alphabets.find(city2[-1])]
-
-    return set(check_road) <= set(str_roads)
+    return set(city1[0] + city2[-1]) <= set(str_roads)
